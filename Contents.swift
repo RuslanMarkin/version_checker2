2eecf3e9faeeaf4cf1542d53ddb5e086c1d799e9
RuslanMarkin <ruslan.markin.work@gmail.com>
2022-12-01 18:15:25 +0300

    Initial

---
+++ Contents.swift
@@ -0,0 +1,44 @@
+import Foundation
+
+
+let current = "02.234"
+let appStore = "02"
+let result = appStore.versionCompare(current)
+
+switch result{
+    
+case .orderedSame:
+    print("Equal")
+case .orderedAscending:
+    print("Ascending")
+case .orderedDescending:
+    print("Descending")
+}
+
+
+
+extension String {
+    func versionCompare(_ otherVersion: String) -> ComparisonResult {
+        let versionDelimiter = "."
+
+        var versionComponents = self.components(separatedBy: versionDelimiter)
+        var otherVersionComponents = otherVersion.components(separatedBy: versionDelimiter)
+
+        let zeroDiff = versionComponents.count - otherVersionComponents.count
+
+        if zeroDiff == 0 {
+            return self.compare(otherVersion, options: .numeric)
+        } else {
+            let zeros = Array(repeating: "0", count: abs(zeroDiff))
+            if zeroDiff > 0 {
+                otherVersionComponents.append(contentsOf: zeros)
+            } else {
+                versionComponents.append(contentsOf: zeros)
+            }
+            return versionComponents.joined(separator: versionDelimiter)
+                .compare(otherVersionComponents.joined(separator: versionDelimiter), options: .numeric)
+        }
+        
+    }
+}
+
